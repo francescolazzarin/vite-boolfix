676ce4f0763f274c54357c93a742c18711035a95
francescolazzarin <francesco.lazzarin1996@gmail.com>
2024-02-16 12:26:36 +0100

    <feat>in cardscontent metodo on over, gestite immagini di copertina non presenti

--- src/components/header/AppHeader.vue
+++ src/components/header/AppHeader.vue
@@ -16,7 +16,7 @@ import { store } from '../../store';
 <template>
    <header class="p-3 bg-dark d-flex justify-content-between align-items-center">
     <h1 class="text-danger">Boolfix</h1>
-    <div class="input-group mb-3 w-25">
+    <div class="input-group w-25">
         <input v-model="store.cerca" type="text" class="form-control" placeholder="Cerca" aria-describedby="button-addon2">
         <button @click="$emit('cercaFilm')" class="btn btn-outline-secondary" type="button" id="button-addon2">Cerca</button>
     </div>
--- src/components/main/CardsContent.vue
+++ src/components/main/CardsContent.vue
@@ -2,12 +2,16 @@
 import { store } from '../../store';
     export default{
         name:'CardsContent',
+        components:{
+        
+        },
         props:{
             propsElement:Object
         },
         data(){
             return{
                 store,
+                visibile:false
             }
         },
         methods:{
@@ -28,38 +32,56 @@ import { store } from '../../store';
             },
             getVote(element){
                 let stars
-                stars=parseInt(element / 2)
+                stars=Math.ceil(element / 2)
                 let arrayStars=Array(stars).fill(0).map(( index) => index +1)
                 return arrayStars.length
             },
+            info(){
+                this.visibile=true
+                console.log('mouse')
+            },
+            nascondi(){
+                this.visibile=false
+            }
         }
     }
 
 </script>
 
 <template>
-    <div class="card col-3 bg-dark text-light">
-        <img :src="`${store.urlImg}/w342${propsElement.poster_path || propsElement.backdrop_path }`" class="card-img-top" alt="...">
-        <div class="card-body">
+    <div class="card col-3 bg-dark text-light position-relative"  @mouseover="info()" @mouseleave="nascondi()" >
+        <div v-if="propsElement.poster_path !== null || propsElement.backdrop_path !== null">
+            <img :src="`${store.urlImg}/w342${propsElement.poster_path || propsElement.backdrop_path }`" class="card-img-top" alt="..." style="height: 350px;">
+        </div>
+        <div v-else>
+            <img class="card-img-top" alt="..." src="../../assets/img/non_presente.jpg" style="height: 350px;">
+        </div>
+        
+        <div  class="position-absolute" v-show="visibile">
             <h5 class="card-title">{{ propsElement.title || propsElement.name }}</h5>
+            <p>voto:{{getVote(propsElement.vote_average)}}
+                <i class=" text-warning fa-solid fa-star"
+                v-for="(element, index) in getVote(propsElement.vote_average)">
+                </i>
+            </p>
+            <p>overview:<span>{{ propsElement.overview.slice(0, 100) }}...</span></p>
+        </div>
+        <div class="card-body">
             <figure>
                 <img :src="`https://flagsapi.com/${changeStrFlag(propsElement.original_language).toUpperCase()}/flat/64.png`"
                 style="width: 75px;
                 height: 50px;"
                 alt="">
-                <p>voto:{{getVote(propsElement.vote_average)}}
-                    <i class=" text-warning fa-solid fa-star"
-                    v-for="(element, index) in getVote(propsElement.vote_average)">
-
-                    </i>
-                </p>
             </figure>
          </div>
+        <!-- <CardsContent/> -->
     </div>
 
 </template>
 
 <style lang="scss" scoped>
-
+  .card:hover img{
+    opacity: 0;
+  }  
 
 </style>
